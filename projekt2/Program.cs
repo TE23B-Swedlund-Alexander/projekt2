9ed36d4249ab65dd50a246ccca70b7069f2e5aee
alexander swedlund <alexander.swedlund@elev.ga.ntig.se>
2025-04-08 13:15:06 +0200

    hrsfgdhkjk

---
+++ projekt2/Program.cs
@@ -0,0 +1,34 @@
+﻿Random rnd = new Random();
+
+
+int guy1Hp = 100;
+int guy2Hp = 100;
+
+static int DmgCalculation()
+{
+    Random rnd = new Random();
+    
+    int dmg = 7;
+    int dmgbonus = rnd.Next(1, 5); //funkar det så funkar det
+    dmg = dmg + dmgbonus;
+    int accurecy = rnd.Next(1, 5);
+    if (accurecy == 1)
+    {
+        dmg = 0;
+        Console.WriteLine("miss");
+    }
+    if (accurecy == 5)
+    {
+        dmg *=2 ;
+Console.WriteLine("critical hit");
+    }
+    
+    return dmg;
+}
+static int HpBar(int dmg,int hp)
+{
+   for(int i=hp)
+
+
+   return hp;
+}
