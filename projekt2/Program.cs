a703de853601b2f9f65d01680f9519339da2df95
alexander swedlund <alexander.swedlund@elev.ga.ntig.se>
2025-04-30 14:46:05 +0200

    hoppas den funkar

--- projekt2/Program.cs
+++ projekt2/Program.cs
@@ -1,8 +1,11 @@
 ﻿Random rnd = new Random();
-
+int guy1Dmg = 1;
+int guy2Dmg = 1;
 int guy1Hp = 100;
 int guy2Hp = 100;
-
+int roundOutcome = 0;//outcome adds a rock paper scissors element
+Console.WriteLine("what is your name");
+string nameGuy1 = Console.ReadLine();
 
 //picks what parts are part of the name
 int nP1 = rnd.Next(0, 5); //first half of first name //stands for namePart1
@@ -15,26 +18,18 @@ List<string> namePart2 = ["lan", "el", "ec", "ca", "anne"]; // second half first
 List<string> lastNamePart1 = ["Yod", "Single", "Farr", "Sto", "Pe"]; // first half last name
 List<string> lastNamePart2 = ["er", "ton", "ell", "kes", "rry"]; // second half last name
 
-string nameGuy1 = namePart1[nP1] + namePart2[nP2] + " " + lastNamePart1[lNP1] + lastNamePart2[lNP2];
-
-
- //picks what parts are part of the other name
-nP1 = rnd.Next(0, 5); //first half of first name
-nP2 = rnd.Next(0, 5); // second half first name
-lNP1 = rnd.Next(0, 5);// first half last name
-lNP2 = rnd.Next(0, 5);// second half last name
 
 string nameGuy2 = namePart1[nP1] + namePart2[nP2] + " " + lastNamePart1[lNP1] + lastNamePart2[lNP2];
 
 
 
 
-static int DmgCalculation(int hp)
+static int BaseDmgCalculation()
 {
     Random rnd = new Random();
 
     int dmg = 7;
-    int dmgbonus = rnd.Next(1, 6); //makes the damage varry from hit to hit
+    int dmgbonus = rnd.Next(1, 6); //makes the damage vary from hit to hit
     dmg = dmg + dmgbonus;
     int accurecy = rnd.Next(1, 21); //crit, miss or hit
 
@@ -49,9 +44,8 @@ static int DmgCalculation(int hp)
         dmg *= 2;
         Console.WriteLine("critical hit");
     }
-    Console.WriteLine($"{dmg} damage"); //makes damage happen
-    hp -= dmg;
-    return hp;
+
+    return dmg;
 }
 static void HpBar(int hp, string name) // makes a hp bar
 {
@@ -71,22 +65,90 @@ static void HpBar(int hp, string name) // makes a hp bar
 
     Console.WriteLine($"]{hp}/100"); //in numbers to make it easier to see exact amount
 
-    
+
+}
+static int AttackOutcome(string name)
+{
+    Random rnd = new Random();
+    int attackPlayer = 0;
+    int attackEnemy = rnd.Next(1, 3);
+    string attackEnemyString = "aaa";
+    int attackOutcome = 1;
+    bool attackChosen = false;
+    if (attackEnemy == 1) attackEnemyString = "punches";
+    if (attackEnemy == 2) attackEnemyString = "kicks";
+    if (attackEnemy == 3) attackEnemyString = "faints";
+    while (attackChosen == false)
+    {
+        Console.WriteLine("what attack do you want to use");
+        Console.WriteLine("kick faint or punch");
+        string playerChoice = Console.ReadLine();
+        playerChoice = playerChoice.ToLower();
+        if (playerChoice == "punch")
+        {
+            attackPlayer = 1;
+        }
+        else if (playerChoice == "kick")
+        {
+            attackPlayer = 2;
+        }
+        else if (playerChoice == "faint")
+        {
+            attackPlayer = 3;
+        }
+        else Console.WriteLine("choose one of the attacks");
+    }
+
+
+    if (attackPlayer == attackEnemy)
+    {
+
+        attackOutcome = 3;
+    }
+
+
+    if (attackPlayer == 1 && attackEnemy == 2 || attackPlayer == 2 && attackEnemy == 3 || attackPlayer == 3 && attackEnemy == 1)
+    {
+
+        attackOutcome = 2;
+    }
+
+
+    if (attackPlayer == 1 && attackEnemy == 3 || attackPlayer == 2 && attackEnemy == 1 || attackPlayer == 3 && attackEnemy == 2)
+    {
+
+        attackOutcome = 1;
+    }
+    Console.WriteLine($"you {attackPlayer} {name}, {name} {attackEnemyString} you");
+
+    return attackOutcome;
 }
 
 
 while (guy1Hp > 0 && guy2Hp > 0) //runs fight
 {
+    roundOutcome = AttackOutcome(nameGuy2);
 
-    Console.WriteLine($"{nameGuy1} attacks {nameGuy2}"); //1 attacks 2
-    guy2Hp = DmgCalculation(guy2Hp);
-    HpBar(guy2Hp, nameGuy2);
+    guy1Dmg = BaseDmgCalculation();
+    guy2Dmg = BaseDmgCalculation();
+
+    if (roundOutcome == 1)
+    {
+        guy1Dmg *= 2;
+        guy2Dmg = 0;
+    }
+    if (roundOutcome == 2)
+    {
+        guy1Dmg = 0;
+        guy2Dmg *= 2;
+    }
+guy1Hp-=guy2Dmg;
+guy2Hp-=guy2Dmg;
 
     Console.ReadLine();
 
-    Console.WriteLine($"{nameGuy2} attacks {nameGuy1}"); //2 attacks 1
-    guy1Hp = DmgCalculation(guy1Hp);
     HpBar(guy1Hp, nameGuy1);
+    HpBar(guy2Hp, nameGuy2);
 
     Console.ReadLine();
 }
--- projekt2/obj/Debug/net8.0/projekt2.AssemblyInfo.cs
+++ projekt2/obj/Debug/net8.0/projekt2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projekt2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+407f0e5d9ff1115f6c51a7d1dc5c57acfd4624b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+042266503dd9349bbdbf5c1a09fcefff1232d546")]
 [assembly: System.Reflection.AssemblyProductAttribute("projekt2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projekt2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
